eafc92bc904e7354ed7f148808c117058645a8ff
Brian Robbins <brianrob@microsoft.com>
2017-05-12 22:49:03 -0700

    Add allocator test.

---
+++ allocator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        const int NumThreads = 2;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Starting {NumThreads} threads.");
+
+            Task[] workers = new Task[NumThreads];
+            for(int i=0; i<NumThreads; i++)
+            {
+                workers[i] = new Task(() => AllocTest.Allocator());
+                workers[i].Start();
+            }
+
+            Console.WriteLine("Waiting for all workers to complete.");
+            Task.WaitAll(workers);
+        }
+    }
+
+    public static class AllocTest
+    {
+        public static void Allocator()
+        {
+            Console.WriteLine($"Started new thread with id {System.Threading.Thread.CurrentThread.ManagedThreadId}.");
+
+            while(true)
+            {
+                object o = new object();
+                GC.KeepAlive(o);
+            }
+        }
+    }
+}
+
