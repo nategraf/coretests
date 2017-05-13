fa6a331bd87c0b77a97f329ac390d90b39d767f0
Brian Robbins <brianrob@microsoft.com>
2017-05-12 23:58:14 -0700

    Add thread_stress test.

---
+++ thread_stress/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace thread_stress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            while(true)
+            {
+                if(count % 100 == 0)
+                {
+                    GC.Collect();
+                }
+
+                Thread t = new Thread(new ThreadStart(Worker));
+                t.Start();
+                t.Join();
+            }
+        }
+
+        private static void Worker()
+        {
+            Console.WriteLine("Thread ID: {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
