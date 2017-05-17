72cf51ff92626cd0c523fe12adf5312328aa6f93
Brian Robbins <brianrob@microsoft.com>
2017-05-17 12:11:06 -0700

    Update task_stress to add more concurrency.

--- task_stress/Program.cs
+++ task_stress/Program.cs
@@ -7,41 +7,56 @@ namespace task_stress
 {
     class Program
     {
+        private static bool s_errorOccurred = false;
+        private static int s_numTasks = 10;
+
         static void Main(string[] args)
         {
-            int numTasks = 10;
             if(args.Length > 0)
             {
-                numTasks = Convert.ToInt32(args[0]);
+                s_numTasks = Convert.ToInt32(args[0]);
             }
             int currentIteration = 1;
-            while(true)
+
+            Thread taskThread = new Thread(new ThreadStart(TaskThreadProc));
+            taskThread.Start();
+
+            while(!s_errorOccurred)
             {
                 Console.WriteLine("Running iteration {0}", currentIteration++);
 
                 // Start tracing.
                 TraceControl.EnableDefault();
 
+                // Sleep for 5 seconds to fill the trace buffer.
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                // Stop tracing.
+                TraceControl.Disable();
+            }
+        }
+
+        private static void TaskThreadProc()
+        {
+            while(!s_errorOccurred)
+            {
                 try
                 {
-                // Start tasks
-                Task[] tasks = GenerateTasks(numTasks);
-                for(int i=0; i<tasks.Length; i++)
-                {
-                    tasks[i].Start();
-                }
+                    // Start tasks
+                    Task[] tasks = GenerateTasks(s_numTasks);
+                    for(int i=0; i<tasks.Length; i++)
+                    {
+                        tasks[i].Start();
+                    }
 
-                // Wait for completion.
-                Task.WaitAll(tasks);
+                    // Wait for completion.
+                    Task.WaitAll(tasks);
                 }
                 catch(Exception ex)
                 {
                     Console.WriteLine(ex.ToString());
-                    break;
+                    s_errorOccurred = true;
                 }
-
-                // Stop tracing.
-                TraceControl.Disable();
             }
         }
 
