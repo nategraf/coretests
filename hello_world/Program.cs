e5620f5640f33f24b9ab098fd15fa89d53ca6124
Brian Robbins <brianrob@microsoft.com>
2017-05-30 11:32:24 -0700

    Update hello_world to allocate.

--- hello_world/Program.cs
+++ hello_world/Program.cs
@@ -6,7 +6,16 @@ namespace hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start: Allocation");
+            DateTime endTime = DateTime.Now + TimeSpan.FromSeconds(10);
+            while(DateTime.Now < endTime)
+            {
+                for(int i=0; i<1000000; i++)
+                {
+                    GC.KeepAlive(new object());
+                }
+            }
+            Console.WriteLine("Stop: Allocation");
         }
     }
 }
